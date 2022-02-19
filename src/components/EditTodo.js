b0c6cd3d6ab87d062a3e0fdd1421bfb741cfa9f8
Diogo Travalha <diogotravalha@gmail.com>
2022-02-19 15:18:21 -0300

    19-02-2022

---
+++ src/components/EditTodo.js
@@ -0,0 +1,87 @@
+import React, { Fragment, useState } from 'react';
+
+const EditTodo = ({ todo }) => {
+    const [description, setDescription] = useState(todo.description);
+
+    async function updateDescription(e) {
+        e.preventDefault();
+        try {
+            const body = { description };
+            const response = await fetch(
+                `https://apiservertravalha.herokuapp.com:3333/todos/${todo.todo_id}`,
+                {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(body),
+                }
+            );
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    return (
+        <Fragment>
+            <button
+                type="button"
+                class="btn btn-warning"
+                data-toggle="modal"
+                data-target={`#id${todo.todo_id}`}
+            >
+                Edit
+            </button>
+
+            <div
+                class="modal"
+                id={`id${todo.todo_id}`}
+                onClick={() => setDescription(todo.description)}
+            >
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">Edit Item</h4>
+                            <button
+                                type="button"
+                                class="close"
+                                data-dismiss="modal"
+                                onClick={() => setDescription(todo.description)}
+                            >
+                                &times;
+                            </button>
+                        </div>
+
+                        <div class="modal-body">
+                            <input
+                                className="form-control"
+                                type="text"
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                            />
+                        </div>
+
+                        <div class="modal-footer">
+                            <button
+                                type="button"
+                                class="btn btn-warning"
+                                data-dismiss="modal"
+                                onClick={(e) => updateDescription(e)}
+                            >
+                                Edit
+                            </button>
+                            <button
+                                type="button"
+                                class="btn btn-danger"
+                                data-dismiss="modal"
+                                onClick={() => setDescription(todo.description)}
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
+};
+
+export default EditTodo;
---
+++ src/components/ListTodo.js
@@ -0,0 +1,76 @@
+import React, { Fragment, useEffect, useState } from 'react';
+
+import EditTodo from './EditTodo';
+
+const ListTodo = () => {
+    const [todos, setTodos] = useState([]);
+
+    async function getTodos() {
+        try {
+            const response = await fetch(
+                'https://apiservertravalha.herokuapp.com:3333/todos'
+            );
+            const jsonData = await response.json();
+            setTodos(jsonData);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    async function deleteTodo(id) {
+        try {
+            const deleteTodo = await fetch(
+                `https://apiservertravalha.herokuapp.com:3333/todos/${id}`,
+                {
+                    method: 'DELETE',
+                }
+            );
+            setTodos(todos.filter((todo) => todo.todo_id !== id));
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getTodos();
+    }, [todos]);
+
+    return (
+        <Fragment>
+            <table class="table mt-5 text-center">
+                <thead>
+                    <tr>
+                        <th>Description</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* <tr>
+                        <td>John</td>
+                        <td>Doe</td>
+                        <td>john@example.com</td>
+                    </tr> */}
+                    {todos.map((todo) => (
+                        <tr key={todo.todo_id}>
+                            <td>{todo.description}</td>
+                            <td>
+                                <EditTodo todo={todo} />
+                            </td>
+                            <td>
+                                <button
+                                    className="btn btn-danger"
+                                    onClick={() => deleteTodo(todo.todo_id)}
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </Fragment>
+    );
+};
+
+export default ListTodo;
